cfdaa05a59fe58f07588e0292489444a8ded2af2
blackmirroxx <olivier.mutschler@gmail.com>
2022-07-29 05:07:35 +0200

    calling functions ... inside print...

--- Algorithms/main.hs
+++ Algorithms/main.hs
@@ -1,4 +1,8 @@
+import Recursion
+
 main = do
     putStrLn "Hello, what is your name ?"
     name <- getLine
     putStrLn ("Hi " ++ name ++ " you rock ")
+    putStrLn ("fact " ++ (show 3) ++ " is " ++ (show (fact 3)))
+    
