96b3fe727d8230d0e0a39862b7cb691e7ba5c1c8
blackmirroxx <olivier.mutschler@gmail.com>
2023-06-21 04:09:15 +0200

    merge sort

--- Algorithms/6_MergeSort.hs
+++ Algorithms/6_MergeSort.hs
@@ -4,12 +4,12 @@ module MergeSort
 msort :: [Int] -> [Int]
 msort []  = []
 msort [x] = [x]
-msort xs  = merge (msort fs) (msort bs)
-    where fs  = take mid xs
+msort xs  = merge (msort fs) (msort bs) -- fs front or left vs bs back or right -> recursive
+    where fs  = take mid xs             -- devide and conquer
           bs  = drop mid xs
           mid = (length xs) `div` 2
 
-merge :: [Int] -> [Int] -> [Int]
+merge :: [Int] -> [Int] -> [Int]        -- here the actual sort is happening
 merge [] ys = ys
 merge xs [] = xs
 merge (x:xs) (y:ys)
